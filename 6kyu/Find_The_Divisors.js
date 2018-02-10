5d008f302792a2d880e944854d65472c05472fee
Suki_Yeung <30331289+suukii@users.noreply.github.com>
2018-02-10 15:08:58 +0800

    Create Find_The_Divisors.js

---
+++ 6kyu/Find_The_Divisors.js
@@ -0,0 +1,9 @@
+function divisors(integer) {
+  var res = [];
+  for (var i = 2; i <= Math.floor(integer / 2); i++) {
+    if (integer % i === 0) {
+      res.push(i);
+    }
+  }
+  return res.length === 0 ? (integer + " is prime") : res;
+};
